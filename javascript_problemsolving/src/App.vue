71b04de4e71764365f25d10ee5052292b76aafa1
Poonam Gahatraj <poonamgtrz@gmail.com>
2023-12-01 18:44:30 +0545

    javascript_problemsolving

---
+++ javascript_problemsolving/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import jspractice from './components/jspractice.vue'
+</script>
+
+<template>
+  <jspractice/>
+</template>
+
+<style scoped>
+</style>
---
+++ javascript_problemsolving/src/components/jspractice.vue
@@ -0,0 +1,44 @@
+<template></template>
+<script>
+let is_array=function(input){
+  if(input instanceof Array)
+    {
+      return true;
+    }
+  else {
+    return false;
+  }
+}
+
+console.log(is_array("hello"))
+console.log(is_array([1,2,3]))
+
+//cloning an array
+
+let array=[1,2,3,4]
+let clone_array=[]
+// clone_array=array
+// console.log(array);
+// console.log(clone_array)
+
+//loop method
+//  for(let i=0; i<=array.length;i++){
+//     clone_array=array
+//  }
+
+//  console.log(array);
+//  console.log(clone_array)
+
+ clone_array=array.slice();
+ console.log(array);
+ console.log(clone_array)
+
+
+ //simple JavaScript program to join all elements of the following array into a string
+
+ let arr =["hello","this","is","Poonam"]
+
+ console.log(arr.join(""))
+ console.log(arr.toString())
+
+</script>
